7364f275d6d091fcbb404374e512a6e7fca78fe2
jin <20220330jin@gmail.com>
2022-06-01 17:25:39 +0900

    [추가] Header 부분 게시판 버튼 추가

---
+++ todo_FE/js/header.js
@@ -0,0 +1,14 @@
+/**
+ * Header 부분 기능 정의
+ */
+
+
+// YJ Board로 이동하는 버튼 클릭시 실행
+function boardYJBind() {
+    alert('미구현된 기능입니다. YJ에게 문의하세요');
+}
+
+// HJ Board로 이동하는 버튼 클릭시 실행
+function boardHJBind() {
+    alert('미구현된 기능입니다. HJ에게 문의하세요');
+}
