d36134fd26c0f7aeca94bb3f5dc531a788b4506d
young jo <a10019610@gmail.com>
2022-06-05 00:49:22 +0900

    [추가] Main_TODO page 화면 - 버튼 구성  , YJBoard 추가

---
+++ todo_FE/js/main.js
@@ -0,0 +1,11 @@
+function todoPlus() {
+  alert("TO-DO 추가하는 버튼 구현중입니다.");
+}
+
+function todoUpdate() {
+  alert("TO-DO 수정하는 버튼 구현중임");
+}
+
+function todoDelite() {
+  alert("TO-DO 삭제하는 버튼 구현중입니다.");
+}
