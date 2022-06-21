b194457ab81fa60e87f719477855c2ce50521dbd
young jo <a10019610@gmail.com>
2022-06-22 01:36:55 +0900

    [수정] '추가' 버튼 통한 텍스트 나열 (리스트) 구현

--- todo_FE/js/main.js
+++ todo_FE/js/main.js
@@ -1,5 +1,21 @@
-function todoPlus() {
-  alert("TO-DO 추가하는 버튼 구현중입니다.");
+let todoInputElement = document.getElementById("todoInput");
+let todoAddBtnElement = document.getElementById("todoBtn");
+let todoListElement = document.getElementById("todoList");
+
+// function inputText() {
+//   let listCreate = document.createElement("li");
+//   listCreate.innerText = todoInputElement.value;
+//   todoListElement.appendChild(listcreate());
+// }
+
+// todoInputElement.addEventListener("click");
+
+todoAddBtnElement.addEventListener("click", addTodo);
+
+function addTodo() {
+  const listCreate = document.createElement("li");
+  listCreate.innerHTML = todoInputElement.value;
+  todoListElement.appendChild(listCreate);
 }
 
 function todoUpdate() {
