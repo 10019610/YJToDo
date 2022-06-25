78c4588b7e3d5426ba56809ceb52b6b780e58619
jin <20220330jin@gmail.com>
2022-06-25 19:54:31 +0900

    [추가] 벡엔드 세팅 파일 추가

---
+++ todo_FE/app.js
@@ -0,0 +1,20 @@
+// import express package
+const express = require('express'); 
+
+// apply express
+const app = express();
+
+// routing root page
+app.get('/', function(req, res){
+
+})
+
+// routing HJBoard page
+app.get('/hjBoard', function(req, res){
+    
+})
+
+// routing YJBoard page
+
+// setting port
+app.listen(3000);
