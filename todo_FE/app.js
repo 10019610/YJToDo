9d760936634e6bb5fe6517bf3ac8f282142b2591
jin <20220330jin@gmail.com>
2022-06-26 17:02:24 +0900

    [수정] 공통 public 파일 이동

--- todo_FE/app.js
+++ todo_FE/app.js
@@ -1,20 +1,30 @@
 // import express package
-const express = require('express'); 
+const express = require('express');
+
+// import path
+const path = require('path');
 
 // apply express
 const app = express();
 
-// routing root page
-app.get('/', function(req, res){
+// css, js 파일을 사용하기 위해 세팅
+app.use(express.static('public'));
 
+// routing root page
+app.get('/', function (req, res) {
+    // 이동하려는 file이 있는 경로 세팅
+    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlFilePath);
 })
 
 // routing HJBoard page
-app.get('/hjBoard', function(req, res){
-    
+app.get('/hjBoard', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'board', 'HJ_Board.html');
+    console.log(__dirname);
+    res.sendFile(htmlFilePath);
 })
 
 // routing YJBoard page
 
 // setting port
-app.listen(3000);
+app.listen(3001);
