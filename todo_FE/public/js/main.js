f58ddefd5c44a4f52ef55db283ad2dda0e27f0e9
young jo <a10019610@gmail.com>
2022-07-05 22:34:01 +0900

    [추가] todo 삭제, 수정 버튼 추가, 기능 구현중(오류 있음), YJboard 게시글 테이블 추가

--- todo_FE/public/js/main.js
+++ todo_FE/public/js/main.js
@@ -8,22 +8,33 @@ let todoListElement = document.getElementById("todoList");
 //   todoListElement.appendChild(listcreate());
 // }
 
-// todoInputElement.addEventListener("click");
 
 todoAddBtnElement.addEventListener("click", addTodo);
 
 function addTodo() {
 	const listCreate = document.createElement("li");
+	// const spanTag = document.createElement("span");
 	listCreate.innerHTML = todoInputElement.value;
 	todoListElement.appendChild(listCreate);
+	todoInputElement.value = '';  // 입력 후 텍스트 비우기.
+
+	const delBtn = document.createElement("button");
+	delBtn.innerText = "삭제";
+	todoListElement.appendChild(delBtn);
+	delBtn.addEventListener("click", function () {
+		todoListElement.removeChild(listCreate)
+	});
+
+	const udtBtn = document.createElement("button");
+	udtBtn.innerText = "수정";
+	todoListElement.appendChild(udtBtn)
+
+
 }
 
 function todoUpdate() {
 	alert("TO-DO 수정하는 버튼 구현중임");
 }
 
-// function todoDelite() {
-// 	const todoContent = document.getElementById('todoList');
-// 	todoListElement.removeChild(todoContent);
-// }
+
 
