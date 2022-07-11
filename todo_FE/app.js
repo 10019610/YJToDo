37a502680d34dfed10137153a9c7677ad43821eb
jin <20220330jin@gmail.com>
2022-07-11 19:55:27 +0900

    [수정] 파일 꼬인거 수정

--- todo_FE/app.js
+++ todo_FE/app.js
@@ -13,8 +13,9 @@ const path = require('path');
 // css, js 파일을 사용하기 위해 세팅
 app.use(express.static('public'));
 
-// routes Approval
+// routes
 const approvalRoutes = require('./routes/approval');
+const loginRoutes = require('./routes/user');
 
 // 폴더 구조 설정
 app.set('views', path.join(__dirname, 'views'));
@@ -23,42 +24,10 @@ app.set('view engine', 'ejs');
 //parsing req.body
 app.use(express.urlencoded({ extended: true }));
 
-// routes Login 
-const loginRoutes = require('./routes/user');
 //parsing req.body
 app.use(express.urlencoded({ extended: true }));
 
 app.use(loginRoutes);
-
-// import express-session 
-const session = require('express-session');
-
-const MySQLStore = require('express-mysql-session')(session);
-
-const options = {
-	host: "144.24.68.69",
-	port: 3306,
-	user: "yang",
-	password: "Rlagus12!@",
-	database: "testdb",
-};
-
-const sessionStore = new MySQLStore(options);
-
-app.use(session({
-	secret: 'super-secret',
-	resave: false,
-	saveUninitialized: true,
-	store: sessionStore,
-}));
-
-
-// routes Approval
-const approvalRoutes = require('./routes/approval');
-
-
-
-
 
 // 결재 라우팅 설정 가져오기
 app.use('/approval', approvalRoutes);
