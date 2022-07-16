69a429620c780c1e03da5b9a005a992224b11e72
young jo <a10019610@gmail.com>
2022-07-16 20:54:35 +0900

    [수정] yjboard 페이지 렌더링

--- todo_FE/temp_react/src/App.js
+++ todo_FE/temp_react/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Mainpage from "./components/views/Mainpage";
+import { Route } from "react-router-dom";
+import YjBoard from "./components/views/YJ_Board";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/yjBoard">
+        <YjBoard />
+      </Route>
+      <Mainpage />
     </div>
   );
 }
---
+++ todo_FE/temp_react/src/components/views/Mainpage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Mainpage = () => {
+  return <div>dddd</div>;
+};
+
+export default Mainpage;
