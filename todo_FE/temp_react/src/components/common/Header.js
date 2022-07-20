7cb6a22eb83e2e2dbac1c439afdcd63a1dbe50c9
jin <20220330jin@gmail.com>
2022-07-20 19:31:40 +0900

    [수정] 헤더 링크 수정, 시작 포트 3002로 변경

--- todo_FE/temp_react/src/components/common/Header.js
+++ todo_FE/temp_react/src/components/common/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="main-header">
-      <Link to="/mainpage">YJHJ</Link>
+      <Link to="/main">YJHJ</Link>
       <nav>
         <ul>
           <li>
--- todo_FE/temp_react/src/components/views/mainDashBoard/MainDashBoardPage.js
+++ todo_FE/temp_react/src/components/views/mainDashBoard/MainDashBoardPage.js
@@ -1,7 +1,36 @@
+import './MainDashBoardPage.css';
+
 const MainDashBoardPage = () => {
+    // 날짜 타입
+    let date = new Date();
+    const month = date.toLocaleString('en-US', { month: 'long' });
+    const day = date.toLocaleString('en-US', { day: '2-digit' });
+    const year = date.getFullYear();
     return (
         <main>
             <h1>DashBoard</h1>
+            <div className='base_form'>
+                {/* 한 개의 글 */}
+                <div className='board-item'>
+                    {/* 제목 부분 */}
+                    <div className='board-item-title'>
+                        <span>Title</span>
+                        <span>{month}/{day}/{year}</span>
+                    </div>
+                    {/* 글 내용 부분 */}
+                    <div className='board-item-content'>
+                        <div>
+                            Content
+                        </div>
+                    </div>
+                    {/* 글 조작 버튼 부분 */}
+                    <div>
+                        <span>등록</span>
+                        <span>수정</span>
+                        <span>삭제</span>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
