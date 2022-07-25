6c65945b1add65a9666bfdda4347b09d9652e56b
jin <20220330jin@gmail.com>
2022-07-26 08:46:52 +0900

    [수정] 대시보드 글등록 기능 추가

--- client/src/components/mainDashBoard/MainDashBoardForm.js
+++ client/src/components/mainDashBoard/MainDashBoardForm.js
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 import "./MainDashBoard.css";
 import styles from "../../css/Common.module.css";
 
 const MainDashBoardForm = () => {
+
+    const createParam = {
+        content: '',
+    }
+
+    // 입력받은 글 내용 셋팅
+    const [content, setContent] = useState('');
+
+    // 
+    const onChangeContent = (e) => {
+        console.log(e.target.value);
+        setContent(e.target.value);
+    }
+
+    const create = async () => {
+        createParam.content = content;
+        const response = await axios.post("http://localhost:8090/mainDashBoard/create", createParam);
+        console.log(response);
+
+    }
 
     function errorHandler() {
         alert('기능 구현중입니다.');
@@ -11,10 +34,10 @@ const MainDashBoardForm = () => {
         <div className={styles.base_form}>
             <div className="board-form">
                 <span className="board-form-input">
-                    <textarea></textarea>
+                    <textarea onChange={onChangeContent} value={content}></textarea>
                 </span>
                 <span>
-                    <button onClick={errorHandler}>등록</button>
+                    <button onClick={create}>등록</button>
                 </span>
             </div>
         </div>
