dd5954c3be4f3020e4a6db17f001d64fc4a733ab
young jo <a10019610@gmail.com>
2022-07-27 09:58:19 +0900

    [추가] yjBoard 게시판 read axios 추가

--- client/src/components/views/YjBoard/YjBoard.js
+++ client/src/components/views/YjBoard/YjBoard.js
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../../../css/Common.module.css";
+import axios from "axios";
+
 // import { useParams } from "react-router-dom";
 
-const YjBoard = () => {
-  // useState test
-  const data = "Hello";
-  const [state, setState] = useState(data);
+function YjBoard() {
+  const [title, setTitle] = useState(null);
 
-  const alertHandler = () => {
-    let newState;
-    if (state === "Hello") {
-      alert("Hello World!");
-    } else {
-      alert("What?");
-    }
-    setState(newState);
+  const readHandler = () => {
+    axios.get("http://localhost:8090/yjBoard/read").then((response) => {
+      setTitle(response.data);
+      console.log(response);
+      console.log(title);
+    });
+    // .catch(console.error("ss"));
   };
-  // console.log("dddd");
 
   return (
     <React.Fragment>
@@ -28,41 +26,13 @@ const YjBoard = () => {
             <Link to="/yjBoard/write">게시물 추가</Link>
           </button>
         </div>
-        <div>
-          <div>{state}</div>
-          <button onClick={alertHandler}>test!</button>
-        </div>
         <div className="">
-          {/* <thead>
-            <th>번호</th>
-            <th>제목</th>
-            <th>작성자</th>
-            <th>작성일</th>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>게시글</td>
-              <td>yj</td>
-              <td>2022-07-18</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>게시글</td>
-              <td>yj</td>
-              <td>2022-07-18</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>게시글</td>
-              <td>yj</td>
-              <td>22.07.25</td>
-            </tr>
-          </tbody> */}
+          <button onClick={readHandler}>test</button>
         </div>
+        <div>{title && title.title}</div>
       </div>
     </React.Fragment>
   );
-};
+}
 
 export default YjBoard;
