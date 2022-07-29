da66de82e1cd9ac3f3cfcf9f38d803e0abb00cf8
jin <20220330jin@gmail.com>
2022-07-29 21:30:12 +0900

    [수정] 결재요청시  페이지 리스트로 리다이렉트

--- client/src/components/views/approval/ApprovalFormPage.js
+++ client/src/components/views/approval/ApprovalFormPage.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useHistory } from 'react-router-dom';
 
 import ApprovalForm from "../../approval/ApprovalForm";
 
 const ApprovalFormpage = () => {
+  const history = useHistory();
+
+  const addApprovalHandler = approval => {
+    console.log(approval);
+    history.push('/approval/approvalList');
+  }
   return (
     <div>
       <h1>Approval Form</h1>
-      <ApprovalForm></ApprovalForm>
+      <ApprovalForm addApproval={addApprovalHandler}></ApprovalForm>
     </div>
   );
 };
