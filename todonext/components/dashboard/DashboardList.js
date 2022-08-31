bd6a22c48999fe495b5c3178b7c7b5a6d635fafa
jin <20220330jin@gmail.com>
2022-08-31 19:56:41 +0900

    [수정] 파일 정리 및 와꾸 추가

---
+++ todonext/components/dashboard/DashboardList.js
@@ -0,0 +1,11 @@
+import classes from './Dashboard.module.css';
+
+function DashboardList() {
+    return (
+        <div>
+            Dashboard..
+        </div>
+    )
+}
+
+export default DashboardList;
