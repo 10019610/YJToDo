059666c59c7f5c9262b0de5e5d9eb9055ba95381
jin <20220330jin@gmail.com>
2022-09-04 19:19:02 +0900

    [수정] 간단한 header 수정

--- todonext/components/layout/MainHeader.js
+++ todonext/components/layout/MainHeader.js
@@ -1,37 +1,38 @@
 import Link from "next/link";
 
+import classes from './MainHeader.module.css'
+import HamburgerMenuIcon from "../icons/HamburgerMenuIcon";
+
 function MainHeader() {
-  return (
-    <header>
-      <div>
-        <Link href="/">YJHJ</Link>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/dashboard">Dashboard(temp)</Link>
-          </li>
-          <li>
-            <Link href="/signup">SignUp</Link>
-          </li>
-          <li>
-            <Link href="/login">Login</Link>
-          </li>
-          <li>
-            <Link href="/yjTodo">YjTodo</Link>
-          </li>
-          <li>
-            <Link href="/yjBoard">YjBoard</Link>
-          </li>
-          <li>
-            <span></span>
-            <span></span>
-            <span></span>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>
+                <Link href="/">YJHJ</Link>
+            </div>
+            <nav className={classes.navigation}>
+                <ul>
+                    <li>
+                        <Link href="/dashboard">Dashboard(temp)</Link>
+                    </li>
+                    <li>
+                        <Link href="/signup">SignUp</Link>
+                    </li>
+                    <li>
+                        <Link href="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link href="/yjTodo">YjTodo</Link>
+                    </li>
+                    <li>
+                        <Link href="/yjBoard">YjBoard</Link>
+                    </li>
+                    <li>
+                        <span className={classes.icon}><HamburgerMenuIcon></HamburgerMenuIcon></span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default MainHeader;
